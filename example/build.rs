c04531e9179b8777b17e8bc15fb096bad8bb4cb8
ngergs <niklas@ngergs.de>
2025-11-22 16:35:45 +0100

    chore: use serde-saphyr for example yaml deserialization (used by build.rs)

--- example/build.rs
+++ example/build.rs
@@ -11,18 +11,18 @@ struct TypeMetaCatchAll {
     pub api_version: String,
     pub kind: String,
     #[serde(flatten)]
-    other: HashMap<String, serde_yaml_ng::Value>,
+    other: HashMap<String, serde_json::Value>,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     fs::create_dir_all("src/generated")?;
     let xrd_raw = fs::read_to_string("schema/xrd.yaml")?;
-    let mut xrd = serde_yaml_ng::from_str::<TypeMetaCatchAll>(&xrd_raw)?;
+    let mut xrd = serde_saphyr::from_str::<TypeMetaCatchAll>(&xrd_raw)?;
     // kopium only supports native k8s crd's but all relevant fields are the same for crossplane xrd's
     // so we can just adjust the apiVersion and Kind
     "apiextensions.k8s.io/v1".clone_into(&mut xrd.api_version);
     "CustomResourceDefinition".clone_into(&mut xrd.kind);
-    let xrd_raw = serde_yaml_ng::to_string(&xrd)?;
+    let xrd_raw = serde_saphyr::to_string(&xrd)?;
 
     let kopium_binary_name = "kopium";
     let mut kopium_cmd = Command::new(kopium_binary_name)
